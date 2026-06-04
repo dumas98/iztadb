94e2f522f77335625289a8a2057ce8f59a46e01a
Daniel Dumas <danieldumas98@gmail.com>
2026-06-03 21:20:12 -0600

    Built first MemTable using sorted map to store values. Added functions GET, PUT and REMOVE including tombstone records for deletes.

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,25 @@
 #include <iostream>
 #include "iztadb.h"
 
+// Test const in functions
+void shout(std::string s) {
+    std::cout << "shout: " << s << std::endl;
+}
+
+void shout2(std::string& s) {
+    std::cout << "shout: " << s << std::endl;
+    s = "HELLO";
+}
+
 int main() {
     std::cout << "Hello, I'm IztaDB!" << std::endl;
-    unordered_map_commands();
+    // unordered_map_commands();
+
+    std::string str1 = "hello";
+    shout(str1);
+
+    shout2(str1);
+    std::cout << str1 << std::endl;
+
     return 0;
 }
---
+++ src/memtable.cpp
@@ -0,0 +1,80 @@
+//
+// Created by Daniel Dumas on 02/06/26.
+//
+
+#include <map>
+#include <string>
+#include <optional>
+
+using namespace std;
+
+// Values in store can only be either an actual VALUE or a TOMBSTONE (for deletes).
+enum class ValueType {
+    VALUE,
+    TOMBSTONE
+};
+
+// Entries consist of the value type and the actual value.
+struct Entry {
+    ValueType type;
+    string value;
+};
+
+/**
+ * @brief In-memory write buffer for IztaDB.
+ * Stores key-value pairs in a sorted map before flushing to disk as an SSTable.
+ * Reads should check the MemTable before consulting SSTables, because it holds
+ * most recent writes.
+ *
+ */
+class MemTable {
+    private:
+    map<string, Entry> table;
+
+    public:
+    MemTable() = default;
+
+    /**
+     * @brief Inserts a key value pair into the table.
+     * Overwrites if key already exists.
+     *
+     * @param key   the key to insert
+     * @param value the value to insert
+     */
+    void put(const string& key, const string& value) {
+        table[key] = { ValueType::VALUE, value};
+    }
+
+    /**
+     * @brief Retrieves the value associated with a given key.
+     * Returns nothing if the key does not exist or has been deleted.
+     *
+     * @param key the key to search for
+     * @return the value if found, nullopt if key does not exist or is a tombstone
+     */
+    optional<string> get(const string& key) {
+        // Search for key first to avoid writing an empty record.
+        auto it = table.find(key);
+
+        // Only return if record wasn't deleted and it exists in table.
+        if ((it != table.end()) && (it->second.type != ValueType::TOMBSTONE)) {
+            // Second is the value.
+            return it->second.value;
+        }
+        return nullopt;
+    }
+
+    /**
+     * @brief Marks a key as deleted by writing a tombstone record.
+     * Does not erase the key.
+     *
+     * @param key the key to delete
+     */
+    void remove(const string& key) {
+        // Search for key first to avoid writing an empty record.
+        auto it = table.find(key);
+        if ((it != table.end()) && (it->second.type == ValueType::VALUE)) {
+            table[key] = { ValueType::TOMBSTONE, ""};
+        }
+    }
+};
