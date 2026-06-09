0760953254a4bc9fbc51f389b5615648454b9bf0
Daniel Dumas <danieldumas98@gmail.com>
2026-06-08 22:58:53 -0600

    Refactored log_writer.cpp, added get_max_segment static method to simplify resolve_active_log_path and using it for new created method get_latest_sequence that will be used for LogWriter upon construction. Added the other types to WALRecord (previously WALTable).

--- src/log_writer.cpp
+++ src/log_writer.cpp
@@ -6,22 +6,21 @@
 using namespace std;
 
 LogWriter::LogWriter() {
-    write_path = resolve_log_path();
+    write_path = resolve_active_log_path();
+    sequence_number = get_latest_sequence();
 }
 
-string LogWriter::resolve_log_path() {
-
+int LogWriter::get_max_segment() {
     string wal_path = "./data/wal";
     int max_file = 0;
 
-    // If directory is empty, return first log file.
+    // No files exist, use zero.
     if (filesystem::is_empty(wal_path)) {
-        return wal_path + "/000001.log";
+        return max_file;
     }
 
     // Iterate through each directory to get max (corresponds to latest file).
-    for (auto const& dir_entry : filesystem::directory_iterator(wal_path)){
-
+    for (auto const& dir_entry : filesystem::directory_iterator(wal_path)) {
         // Filter out non .log files.
         if (dir_entry.path().extension() != ".log") continue;
 
@@ -33,19 +32,58 @@ string LogWriter::resolve_log_path() {
         }
     }
 
+    return max_file;
+}
+
+string LogWriter::resolve_active_log_path(uintmax_t max_size) {
+    string wal_path = "./data/wal";
+    int max_file = get_max_segment();
+
+    // No files exist, start fresh.
+    if (max_file == 0) {
+        return wal_path + "/000001.log";
+    }
+
     // Build latest file path.
     string latest_file_path = wal_path + "/" + format("{:06d}.log", max_file);
 
     // Return latest file path if file size is smaller than threshold.
-    if (filesystem::file_size(latest_file_path) < MAX_WAL_FILE_SIZE) {
+    if (filesystem::file_size(latest_file_path) < max_size) {
         return latest_file_path;
     }
 
     // Add a one to max file so a new file can be created.
     return wal_path + "/" + format("{:06d}.log", max_file + 1);
-
 }
 
 string LogWriter::get_write_path() {
     return write_path;
 };
+
+uint32_t LogWriter::get_latest_sequence() {
+    int max_file = get_max_segment();
+
+    // If no files exist, start at 1.
+    if (max_file == 0) {
+        return 1;
+    }
+
+    // Open latest segment.
+    string latest_path = "./data/wal/" + format("{:06d}.log", max_file);
+    ifstream file(latest_path, ios::binary);
+
+    // Throw error if opening file fails.
+    if (!file.is_open()) {
+        throw runtime_error("Failed to open WAL file: " + latest_path);
+    }
+
+    // Jump to start of last record, convert first to signed int.
+    file.seekg(-static_cast<int>(sizeof(WALRecord)), ios::end);
+
+    // Read only the sequence number (first 4 bytes), convert sequence address
+    // to the first char (byte).
+    uint32_t sequence_number;
+    file.read(reinterpret_cast<char*>(&sequence_number),sizeof(uint32_t));
+
+    return sequence_number + 1;
+}
--- src/log_writer.h
+++ src/log_writer.h
@@ -2,11 +2,12 @@
 // Created by Daniel Dumas on 05/06/26.
 //
 
-#include <cstdint>
+
 #include <string>
 #include <filesystem>
+#include <fstream>
 
-const uintmax_t MAX_WAL_FILE_SIZE = 10; // Small size to test.
+const uintmax_t MAX_WAL_FILE_SIZE = 100; // Small size to test.
 
 /**
  * @brief Manages write operations to the Write-Ahead Log (WAL).
@@ -23,6 +24,7 @@ const uintmax_t MAX_WAL_FILE_SIZE = 10; // Small size to test.
 class LogWriter {
     private:
         std::string write_path;
+        uint32_t sequence_number;
 
     public:
         /**
@@ -30,10 +32,16 @@ class LogWriter {
          * Calls resolve_log_path to open the active log file.
          */
         LogWriter();
+
+        /**
+         * @brief Returns the highest numbered log segment in the WAL directory.
+         * @return Max segment number, 0 if directory is empty.
+         */
+        static int get_max_segment();
+
         /**
          * @brief Resolves the path of the active log file.
-         * Scans the WAL directory for the highest numbered file and
-         * returns the path to it,
+         * Uses get_max_segment to calculate the maximum segment.
          *
          * If active log file exceeds MAX_WAL_FILE_SIZE the new
          * path will be active log file + 1:
@@ -42,7 +50,7 @@ class LogWriter {
          *
          * @return Full path to the active log file.
          */
-        static std::string resolve_log_path();
+        static std::string resolve_active_log_path(uintmax_t max_size = MAX_WAL_FILE_SIZE) ;
 
         /**
          * @brief Returns the path of the active log file for writing.
@@ -50,10 +58,39 @@ class LogWriter {
          * @return Full path to the active log file.
          */
         std::string get_write_path();
+
+        /**
+         * @brief Retrieves the next available sequence number.
+         *
+         * If no WAL segments exist, returns 1. Otherwise, opens the latest
+         * segment, goes to the last record, and returns its sequence
+         * number plus one.
+         *
+         * @return Next sequence number to use for the upcoming write.
+         * @throws std::runtime_error if the latest WAL segment cannot be opened.
+         */
+        static uint32_t get_latest_sequence();
 };
 
-struct WALFile {
-    uint32_t sequence_number; // 4 Bytes.
+/**
+ * @brief Represents a single record in the Write-Ahead Log.
+ *
+ * Fixed size layout of 393 bytes per record. Padding is disabled
+ * via pragma pack to ensure exact byte layout on disk.
+ * Sequence_number is an increasing sequence to identify record,
+ * CRC32 checksum is used to detect corruption on recovery, type
+ * indicates it's value of ValueType.
+ *
+ * Keys will only support strings with a maximum of 128 chars
+ * and values with a maximum of 256 chars.
+ */
+#pragma pack(1)
+struct WALRecord {
+    uint32_t sequence_number;  // 4 bytes
+    uint32_t checksum;         // 4 bytes
+    uint8_t type;              // 1 byte
+    char key[128];             // 128 bytes
+    char value[256];           // 256 bytes
 };
-
+#pragma pack()
 
